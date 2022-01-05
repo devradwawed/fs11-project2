457a10e8d3bf0a45f94c9e3ed320143867dc1857
RadwaAwed <dev.radwaawed@gmail.com>
2022-01-05 23:31:46 +0200

    tesr

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,4 +15,24 @@ $(document).ready(()=>{
   $(window).scroll(function(){
     $(".navbar").toggleClass("scrolled" ,$(this).scrollTop() > 50)
   })
+  $('.project .owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:3,
+            nav:true,
+            loop:false
+        }
+    }
+})
 })
