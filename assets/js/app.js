7ea7c80cd12b4af2ff9310093848b99aad9d0e09
RadwaAwed <dev.radwaawed@gmail.com>
2022-01-11 00:40:01 +0200

    tesr

--- assets/js/app.js
+++ assets/js/app.js
@@ -55,4 +55,50 @@ $('.team .owl-carousel').owlCarousel({
       }
   }
 })
+$('.blog .owl-carousel').owlCarousel({
+  loop:true,
+  margin:25,
+  responsiveClass:true,
+  responsive:{
+      0:{
+          items:1,
+          nav:true
+      },
+      600:{
+          items:2,
+          nav:true
+      },
+
+      1000:{
+          items:3,
+          nav:true,
+          loop:true
+      }
+  }
+})
+$('.brand .owl-carousel').owlCarousel({
+  loop:true,
+  margin:25,
+  responsiveClass:true,
+  responsive:{
+      0:{
+          items:1,
+          nav:true
+      },
+      600:{
+        items:2,
+        nav:true
+    },
+      1000:{
+          items:3,
+          nav:true
+      },
+      
+      1200:{
+          items:5,
+          nav:true,
+          loop:true
+      }
+  }
+})
 })
